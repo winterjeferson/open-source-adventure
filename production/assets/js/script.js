cf2cafca20030aaedb8ab3797030943774cf6ca6
winterjeferson <winterjeferson@gmail.com>
2020-11-24 22:41:35 -0300

    wireframe

--- production/assets/js/script.js
+++ production/assets/js/script.js
@@ -1,1 +1,1 @@
-class Management{verifyLoad(){window.addEventListener("load",this.build(),{once:!0})}build(){console.log("loaded"),window.theme.build()}}class Theme{constructor(){}build(){console.log("theme build")}}window.management=new Management,window.theme=new Theme,management.verifyLoad();
+class Management{verifyLoad(){window.addEventListener("load",this.build(),{once:!0})}build(){console.log("loaded"),window.theme.build()}}class Theme{build(){console.log("theme build")}}window.management=new Management,window.theme=new Theme,management.verifyLoad();
