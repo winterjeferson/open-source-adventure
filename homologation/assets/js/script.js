c4755f98bbd5c0b41c19c289bd44407ba36782cd
winterjeferson <winterjeferson@gmail.com>
2020-11-24 22:40:47 -0300

    wireframe

---
+++ homologation/assets/js/script.js
@@ -0,0 +1,19 @@
+class Management {
+    verifyLoad() {
+        window.addEventListener('load', this.build(), { once: true });
+    }
+
+    build() {
+        console.log('loaded');
+        window.theme.build();
+    }
+}
+class Theme {
+    build() {
+        console.log('theme build');
+    }
+}
+window.management = new Management();
+window.theme = new Theme();
+
+management.verifyLoad();
